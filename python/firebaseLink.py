f558bb5a08b78eb67ae4be84cfc0ab547c7f2082
Jash's Mac <joshuaravishankar@gmail.com>
2022-05-10 18:07:11 +0900

    Changed names

--- python/firebaseLink.py
+++ python/firebaseLink.py
@@ -2,18 +2,25 @@ import pyrebase
 
 ## Initialization
 
-firebaseConfig = {
-  "apiKey": "AIzaSyDsuqIErWhkKb3gPa1RkAITyaVmaIF1XeQ",
-  "authDomain": "bingbong-7edf4.firebaseapp.com",
-  "databaseURL": "",
-  "projectId": "bingbong-7edf4",
-  "storageBucket": "bingbong-7edf4.appspot.com",
-  "serviceAccount": "./bingbong_firebase_key.json"
-}
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-storage = firebase.storage()
-all_files = storage.list_files()
+# A Sample class with init method
+class FirebaseLink:
+   
+    # init method or constructor 
+    def __init__(self):
+        config = {
+            "apiKey": "AIzaSyDsuqIErWhkKb3gPa1RkAITyaVmaIF1XeQ",
+            "authDomain": "bingbong-7edf4.firebaseapp.com",
+            "databaseURL": "",
+            "projectId": "bingbong-7edf4",
+            "storageBucket": "bingbong-7edf4.appspot.com",
+            "serviceAccount": "./bingbong_firebase_key.json"
+        }
+        firebase = pyrebase.initialize_app(config)
+        self.storage = firebase.storage()
+        self.db = firebase.database()
 
-for file in all_files:
-	file.download_to_filename(file.name.split('/')[-1])
+    def get_all_files(self):
+        return self.storage.list_files()
+
+
